f93c984b61bcfefeb86974555118f33224d7b0a6
jzhengwang@gmail.com <jzhengwang@gmail.com>
2021-02-11 22:20:32 -0800

    WZ:cosmetic change for test

--- python_port_apis/platform_util_functions.py
+++ python_port_apis/platform_util_functions.py
@@ -5,7 +5,8 @@ import sys
 class platform_util:
     cpu_name: object
     pc_cpu_tuple = ("AMD", "x86_64")
-    def __init__(self):
+
+    def __init__ ( self ):
         self.os_name = platform.uname().system
         self.os_version = platform.uname().version
         self.os_machine = sys.platform
@@ -15,39 +16,39 @@ class platform_util:
         self.system_ver = sys.version
         self.system_hw = sys.platform
 
-    def is_platform_pi(self):
+    def is_platform_pi ( self ):
         if self.cpu_name == "aarch64":
             return True
         else:
             return False
 
-    def is_platform_windows(self):
+    def is_platform_windows ( self ):
         if self.os_name == "Windows":
             return True, self.os_machine
         else:
             return False
 
-    def is_platform_linux(self):
+    def is_platform_linux ( self ):
         if self.os_name == "Linux":
             return True, self.os_machine
         else:
             return False
 
-    def is_platform_pc(self):
+    def is_platform_pc ( self ):
         for cpu_name in self.pc_cpu_tuple:
             if self.cpu_name == cpu_name:
                 return True
             else:
                 return False
 
-    def get_node_name(self):
+    def get_node_name ( self ):
         return self.node_name
 
-    def get_node_sys_info(self):
+    def get_node_sys_info ( self ):
         return self.system_hw, self.system_ver
 
-    def get_python_version(self) -> object:
+    def get_python_version ( self ) -> object:
         return self.py_version
 
-    def get_cpu_name(self):
+    def get_cpu_name ( self ):
         return self.cpu_name
